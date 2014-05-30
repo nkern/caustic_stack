7bd3d46f38ecc7da0d6fe10331d949dd4b80b938
nkern <ubuntu@torque-headnode-14758420-04-16-14.novalocal>
2014-05-30 15:54:23 -0500

    new file:   analysis.py

---
+++ analysis.py
@@ -0,0 +1,24 @@
+"""
+-----------
+analysis.py
+-----------
+
+This code implements additional analysis tools that can be used with the stacking technique.
+This includes:
+- Mass mixing
+- Bootstrapping
+- Center offsets
+
+"""
+
+
+
+
+
+
+
+
+
+
+
+
